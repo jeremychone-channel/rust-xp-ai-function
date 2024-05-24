3d28eafbf8fda824d32f3efdbef7cfd84f3b1c08
Jeremy Chone <jeremy.chone@gmail.com>
2024-05-24 16:46:06 -0700

    . added model gpt-4o, examples work

--- src/gpts.rs
+++ src/gpts.rs
@@ -4,6 +4,10 @@ pub const MODEL: &str = MODEL_3_TURBO;
 
 // -- GPT 4 Turbo
 
+// Should use this one if want to use 4
+pub const MODEL_4_O: &str = "gpt-4o";
+
+// Should be legacy model at this point
 // Typically point to the latest (as of 2024-03-13 - "gpt-4-0125-preview")
 pub const MODEL_4_TURBO: &str = "gpt-4-turbo-preview";
 
